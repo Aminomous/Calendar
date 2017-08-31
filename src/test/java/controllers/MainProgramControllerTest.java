36bdcbe0943e35270e226a9b05d6748d7c39beb8
Aminomous <yoyothunder@gmail.com>
2017-08-31 23:06:10 +0700

    edit name's comment in some file add some juint test but error waiting non error jUnit

--- src/test/java/controllers/MainProgramControllerTest.java
+++ src/test/java/controllers/MainProgramControllerTest.java
@@ -3,14 +3,19 @@ package controllers;
  * Thanadon Pakawatthippoyom 5810405037
  */
 import models.Day;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MainProgramControllerTest {
-    @Test
-    public void createAnAppointmentTest(){
-        Day day1 = new Day(1);
+
+    private static Day day1;
+    private static Day day2;
+    @BeforeClass
+    public static void setUp(){
+        day1 = new Day(1);
         String[] temp1 = new String[3];
         temp1[0] = "test1";
         temp1[1] = "test1";
@@ -23,7 +28,12 @@ public class MainProgramControllerTest {
         controller2.initialize();
         controller2.setCurrentDay(1);
         controller2.helperAddAppointment(temp1);
-        Day day2 = controller2.getcurrentDay();
+        day2 = controller2.getcurrentDay();
+    }
+
+    @Test
+    public void createAnAppointmentTest(){
+
 
         assertEquals(day1, day2);
 
